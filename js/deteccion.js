ab52c18249d7f8c615036b550c658247cb5fb896
Diego A Cruz <dacruzt@example.com>
2021-04-26 21:25:16 -0500

    videos ios y android

--- js/deteccion.js
+++ js/deteccion.js
@@ -1,18 +1,14 @@
-   
-    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-console.log(userAgent)
-    if (/android/i.test(userAgent)) {
-         document.getElementsByClassName('ios')[0].style.visibility = 'hidden';
-        document.getElementsByClassName('ios')[0].style.display = 'none';
-    }
-    else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
-         document.getElementsByClassName('android')[0].style.visibility = 'hidden';
-        document.getElementsByClassName('android')[0].style.display = 'none';
-    }
+   var userAgent = navigator.userAgent || navigator.vendor || window.opera;
 
-    else{
-        document.getElementsByClassName('ios')[0].style.visibility = 'hidden';
-        document.getElementsByClassName('ios')[0].style.display = 'none';
-        document.getElementsByClassName('android')[0].style.visibility = 'hidden';
-        document.getElementsByClassName('android')[0].style.display = 'none';
-    }
+   if (/android/i.test(userAgent)) {
+       document.getElementsByClassName('ios')[0].style.visibility = 'hidden';
+       document.getElementsByClassName('ios')[0].style.display = 'none';
+   } else if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+       document.getElementsByClassName('android')[0].style.visibility = 'hidden';
+       document.getElementsByClassName('android')[0].style.display = 'none';
+   } else {
+       document.getElementsByClassName('ios')[0].style.visibility = 'hidden';
+       document.getElementsByClassName('ios')[0].style.display = 'none';
+       document.getElementsByClassName('android')[0].style.visibility = 'hidden';
+       document.getElementsByClassName('android')[0].style.display = 'none';
+   }
